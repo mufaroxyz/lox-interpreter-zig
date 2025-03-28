f19fe8cce43604a08807bdcb1cb81d1665ed0c13
mufaroxyz <mufaro.priv@gmail.com>
2025-03-28 01:12:43 +0100

    Test run

--- src/scanner.zig
+++ src/scanner.zig
@@ -194,7 +194,7 @@ pub const Scanner = struct {
         const value = self.source[self.start..self.current];
         const floatPtr = try std.fmt.parseFloat(f64, value);
         const literalValue = Value.fromNumber(floatPtr);
-        std.debug.print("number: {s}\n", .{literalValue});
+        std.debug.print("number: {s}\n", .{floatPtr});
         try self.addTokenWithLiteral(.NUMBER, literalValue);
     }
 };
